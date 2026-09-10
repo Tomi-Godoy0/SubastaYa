f043da6d36817fe7d1d7d1acb94fb3bf3c01a13d
Tomi-Godoy0 <tomasgodoy037@gmail.com>
2026-09-10 08:44:31 -0300

    feat: agregar middleware y excepciones de dominio

---
+++ Backend/SubastaYa.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubastaYa.Domain.Exceptions;
+using System.Data;
+using System.Net;
+
+namespace SubastaYa.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger; //Registra los errores tecnicos de nuestra aplicacion (es decír de errores de producción) y limpia la información que se le muestra al usuario final.
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext ctx)
+        {
+            try
+            {
+                await _next(ctx);
+
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex ,"Ocurrió un error interno no controlado");
+                await HandleExceptionAsync(ctx, ex);
+
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext ctx, Exception ex)
+        {
+            ctx.Response.ContentType = "application/json";
+
+            //Vamos a mapear códigos de excepciones a códigos HTTP
+            int statusCode = ex switch
+            {
+                //Bad Request
+                ArgumentNullException => StatusCodes.Status400BadRequest,
+                ArgumentException => StatusCodes.Status400BadRequest,
+                // Not Found
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                // Unauthorized
+                UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                // Conflict
+                DuplicateNameException => StatusCodes.Status409Conflict,
+                ConflictException => StatusCodes.Status409Conflict,
+                DbUpdateConcurrencyException => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            string message = statusCode == StatusCodes.Status500InternalServerError ? "Ocurrió un error interno en el servidor." : ex.Message;
+
+            ctx.Response.StatusCode = statusCode;
+
+            var response = new
+            {
+                StatusCode = statusCode,
+                Message = ex.Message
+            };
+
+            var json = System.Text.Json.JsonSerializer.Serialize(response);
+            return ctx.Response.WriteAsync(json);
+        }
+    }
+}
