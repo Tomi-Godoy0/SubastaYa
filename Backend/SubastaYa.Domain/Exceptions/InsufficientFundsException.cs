52f7191acd4d89b190fbb7726e62b55240dab719
Tomi-Godoy0 <tomasgodoy037@gmail.com>
2026-09-16 09:42:26 -0300

    refactor: limpiar using de Domain

--- Backend/SubastaYa.Domain/Exceptions/InsufficientFundsException.cs
+++ Backend/SubastaYa.Domain/Exceptions/InsufficientFundsException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SubastaYa.Domain.Exceptions
+﻿namespace SubastaYa.Domain.Exceptions
 {
     public class InsufficientFundsException : DomainException
     {
