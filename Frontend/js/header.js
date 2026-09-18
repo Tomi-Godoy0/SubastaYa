044aaf86fe5bbb66c8b0c1ffb04a49e8adf597e9
Tomi-Godoy0 <tomasgodoy037@gmail.com>
2026-09-18 05:11:39 -0300

    refactor: eliminar codigo innecesario

--- Frontend/js/header.js
+++ Frontend/js/header.js
@@ -1,19 +1,19 @@
-const usuario = localStorage.getItem("userId");
+document.addEventListener("DOMContentLoaded", () => {
+    const usuario = localStorage.getItem("userId");
+    const usuarioNombre = document.getElementById("usuario-nombre");
+    const btnLogout = document.getElementById("btn-logout");
+    const btnLogin = document.getElementById("btn-login");
+    const btnMiCuenta = document.querySelector(".header__nav a[href*='dashboard']");
 
-const usuarioNombre = document.getElementById("usuario-nombre");
-const btnLogout = document.getElementById("btn-logout");
-const btnLogin = document.getElementById("btn-login");
-const btnMiCuenta = document.querySelector(".header__nav a[href*='dashboard']");
-
-if (usuario) {
-    usuarioNombre.style.display = "inline";
-    btnLogout.style.display = "inline";
-    btnLogin.style.display = "none";
-    btnMiCuenta.style.display = "inline";
-}
-else {
-    usuarioNombre.style.display = "none";
-    btnLogout.style.display = "none";
-    btnLogin.style.display = "inline";
-    btnMiCuenta.style.display = "none";
-}
+    if (usuario) {
+        usuarioNombre.style.display = "inline";
+        btnLogout.style.display = "inline";
+        btnLogin.style.display = "none";
+        btnMiCuenta.style.display = "inline";
+    } else {
+        usuarioNombre.style.display = "none";
+        btnLogout.style.display = "none";
+        btnLogin.style.display = "inline";
+        btnMiCuenta.style.display = "none";
+    }
+});
